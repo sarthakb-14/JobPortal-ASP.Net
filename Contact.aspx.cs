3a78f10a314504a66287ff985866938496fb5c3c
Devanshsati <thedevanshsati@gmail.com>
2024-09-26 17:40:02 +0530

    Constact added and UT deleted

--- Contact.aspx.cs
+++ Contact.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace JobPortal
 {
-    public partial class Contact_us : System.Web.UI.Page
+    public partial class Contact : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
