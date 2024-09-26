b1224cb24d01004392f19568b9ccef623423c78a
utkarshsinghhhhh <veds79108@gmail.com>
2024-09-26 17:30:26 +0530

    contactus changed(linked to navbar on landing page)

--- Contactus.aspx.cs
+++ Contactus.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace JobPortal
 {
-    public partial class Contact : Page
+    public partial class Contact_us : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
